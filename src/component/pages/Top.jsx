11d3f52dea54edcadb3936851eb60f2e101af2ca
anchan <62826339+toshiki-anraku@users.noreply.github.com>
2022-05-14 16:50:11 +0900

    Recoil

--- src/component/pages/Top.jsx
+++ src/component/pages/Top.jsx
@@ -1,12 +1,16 @@
+// import { useContext } from "react";
 import styled from "styled-components";
 import { useHistory } from "react-router-dom";
+import { useSetRecoilState } from "recoil";
+
 import { SecondaryButton } from "../atoms/button/SecondaryButton";
-import { useContext } from "react";
-import { UserContext } from "../../providers/UserProvider";
+// import { UserContext } from "../../providers/UserProvider";
+import { userState } from "../../store/userState";
 
 export const Top = () => {
   const history = useHistory();
-  const { setUserInfo } = useContext(UserContext);
+  // const { setUserInfo } = useContext(UserContext);
+  const setUserInfo = useSetRecoilState(userState);
 
   const onClickAdmin = () => {
     setUserInfo({ isAdmin: true });
